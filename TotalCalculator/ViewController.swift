90fe019f87c851f17985e2448def219fbb892de0
omercsx <omercagris@gmail.com>
2024-11-26 17:34:24 -0500

    Initial Commit

---
+++ TotalCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TotalCalculator
+//
+//  Created by Omer Cagri Sayir on 26.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

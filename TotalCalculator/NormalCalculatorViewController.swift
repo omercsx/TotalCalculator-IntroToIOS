0aec812a4a81a9c163882bede30475b20b3f4298
omercsx <omercagris@gmail.com>
2024-11-29 13:54:49 -0500

    Normal calculator storyboard added

---
+++ TotalCalculator/NormalCalculatorViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NormalCalculatorViewController.swift
+//  TotalCalculator
+//
+//  Created by Omer Cagri Sayir on 29.11.2024.
+//
+
+import UIKit
+
+class NormalCalculatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
